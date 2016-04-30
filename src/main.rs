80c1acac8f16bd1e15f899cb124d898046cdbb2c
Patrick <mptm98@gmail.com>
2016-04-29 22:45:00 -0400

    Changed color and line thickness

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,19 @@ fn sin_x(x: f64) -> f64 {
 }
 
 fn main() {
-    const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-    let window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480))
+    const PRETTY_BLUE: [f32; 4] = [0.0, 0.3, 1.0, 1.0]; // rgba
+    let window: PistonWindow = WindowSettings::new("Sine Wave generator", (640, 480))
                                    .exit_on_esc(true)
                                    .build()
                                    .unwrap_or_else(|e| panic!("Failed:, {}", e));
     for e in window {
         e.draw_2d(|c, g| {
-            clear([0.5, 1.0, 0.5, 1.0], g);
-            g.clear_stencil(0);
+            clear([1.0, 1.0, 1.0, 1.0], g);
+            g.clear_stencil(1);
             let mut x = 0.0;
             while x < 680.0 {
-                Rectangle::new(RED).draw([x, sin_x(x), 1.0, 1.0], &c.draw_state, c.transform, g);
+                Rectangle::new(PRETTY_BLUE)
+                    .draw([x, sin_x(x), 2.0, 2.0], &c.draw_state, c.transform, g);
                 x += 1.0;
             }
         });
